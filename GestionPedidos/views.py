93266fefafa3518d7a0a9a6b6b019092dd3c3e8e
Sergio Sanchez <richardgodplay@gmail.com>
2021-02-24 17:42:48 -0500

    Formularios

---
+++ GestionPedidos/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from GestionPedidos.models import Articulos
+
+# Create your views here.
+
+def busquedaProductos(request):
+
+    return render(request, "busqueda_productos.html")
+
+
+def buscar(request):
+
+    if request.GET["prd"]:
+
+        mensaje = "Articulo Encontrado: %r" %request.GET["prd"]
+
+        producto = request.GET["prd"]
+
+        if len(producto) > 20:
+
+             mensaje = "Texto de busqueda demasiado largo"
+
+        else:
+            
+            articulos = Articulos.objects.filter(nombre__icontains = producto)
+
+            return render (request, "resultados_busqueda.html", {"articulos":articulos, "query": producto})
+    
+    else:
+
+        mensaje = "No has introducido ningun termino de busqueda"
+
+    return HttpResponse(mensaje)
+
+
+def contacto(request):
+
+    if request.method == "POST":
+
+        return render(request, "gracias.html")
+
+    return render(request, "contacto.html")
